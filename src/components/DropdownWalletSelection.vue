66162fa8c1158dcd00607294aea2ec32e2b9db30
Gaël Marcadet <gamarcadet@gmail.com>
2026-03-22 10:24:09 +0100

    feat: add wallet selection Dropdown and enhance WalletRequest approval flow
    Introduces `DropdownWalletSelection` component for selecting wallets and integrates it into the WalletRequest approval workflow. Displays microblock details and virtual blockchain context for approval events.

---
+++ src/components/DropdownWalletSelection.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import Dropdown from "primevue/dropdown";
+import {useStorageStore, WalletEntity} from "../stores/storage.ts";
+import {storeToRefs} from "pinia";
+import {computed, Ref, ref} from "vue";
+
+
+const props = defineProps<{
+  wallets: WalletEntity[],
+  chosenWallet: WalletEntity
+}>();
+const chosenWallet = props.chosenWallet
+</script>
+
+<template>
+  <Dropdown
+      id="walletSelect"
+      v-model="chosenWallet"
+      :options="wallets"
+      optionLabel="name"
+      placeholder="Choose a wallet for auth"
+      class="w-full"
+  >
+    <template #value="slotProps">
+      <div v-if="slotProps.value" class="flex items-center gap-2">
+        <i class="pi pi-wallet text-surface-500"></i>
+        <span>{{ slotProps.value.name }}</span>
+      </div>
+      <span v-else class="text-surface-500">{{ slotProps.placeholder }}</span>
+    </template>
+    <template #option="slotProps">
+      <div class="flex items-center gap-2">
+        <i class="pi pi-wallet text-surface-500"></i>
+        <div>
+          <div class="font-semibold">{{ slotProps.option.name }}</div>
+          <div class="text-xs text-surface-500">{{ slotProps.option.nodeEndpoint }}</div>
+        </div>
+      </div>
+    </template>
+  </Dropdown>
+</template>
--- src/components/walletRequest/WalletRequestEventApproval.vue
+++ src/components/walletRequest/WalletRequestEventApproval.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
 import {
-  CryptoEncoderFactory,
+  ApplicationLedgerVb,
+  CryptoEncoderFactory, EncoderFactory, Microblock, ProviderFactory,
   SeedEncoder,
-  WalletCrypto,
-  WalletRequestDataApproval
+  WalletCrypto, WalletInteractiveAnchoringResponseApprovalData,
+  WalletRequestDataApproval, wiExtensionWallet
 } from "@cmts-dev/carmentis-sdk/client";
-import { ref } from "vue";
 import Card from "primevue/card";
-import Button from "primevue/button";
-import Dropdown from "primevue/dropdown";
+import {onMounted, ref} from "vue";
 import {useStorageStore} from "../../stores/storage.ts";
 import {storeToRefs} from "pinia";
+import WalletRequestEventApprovalWallet from "./WalletRequestEventApprovalWallet.vue";
 
 // load all wallets
 const store = useStorageStore();
@@ -29,7 +29,15 @@ const emit = defineEmits<{
   reject: []
 }>();
 
+// we instantiate the wallet extension
+const wiWallet = new wiExtensionWallet();
+
+
+// we define below the state of the component
 const isProcessing = ref(false);
+const approvalData = ref<WalletInteractiveAnchoringResponseApprovalData | null>(null);
+const microblockToApprove = ref<Microblock | null>(null);
+const virtualBlockchainContainingMicroblock = ref<ApplicationLedgerVb | null>(null);
 
 async function approve() {
 
@@ -38,6 +46,47 @@ async function approve() {
 function reject() {
   emit('reject');
 }
+
+onMounted(async () => {
+  try {
+
+
+    // we obtain the approval data from the server
+    const walletCrypto = WalletCrypto.generateWallet();
+    const accountCrypto = walletCrypto.getDefaultAccountCrypto();
+    const { serverUrl, anchorRequestId } = props.walletRequest;
+    approvalData.value = await wiWallet.getApprovalData(
+        accountCrypto,
+        {serverUrl, anchorRequestId}
+    );
+    const encodedMicroblock = approvalData.value.b64SerializedMicroblock;
+    const rawMicroblock = EncoderFactory.bytesToBase64Encoder().decode(encodedMicroblock);
+    const mb = Microblock.loadFromSerializedMicroblock(rawMicroblock);
+    microblockToApprove.value = mb;
+    console.log("Approval data:", approvalData.value);
+
+
+
+    // we compute the application ledger
+    const nodeUrl = 'https://ares.testnet.carmentis.io'
+    const provider = ProviderFactory.createInMemoryProviderWithExternalProvider(nodeUrl);
+    let applicationLedger = mb.getHeight() === 1 ?
+        ApplicationLedgerVb.createApplicationLedgerVirtualBlockchain(provider) :
+        await provider.loadApplicationLedgerVirtualBlockchain(
+            await provider.getVirtualBlockchainIdContainingMicroblock(
+                // we are looking for the previous microblock hash, the received one is not anchored yet
+                mb.getPreviousHash()
+            )
+        );
+    applicationLedger.enableDraftMode();
+    await applicationLedger.appendMicroBlock(mb);
+    virtualBlockchainContainingMicroblock.value = applicationLedger;
+
+  } catch (e) {
+    console.error("Error obtaining approval data:", e);
+    reject()
+  }
+})
 </script>
 
 <template>
@@ -46,4 +95,52 @@ function reject() {
   </div>
   <p>{{props.walletRequest.serverUrl}}</p>
   <p>{{props.walletRequest.anchorRequestId}}</p>
+
+
+  <div class="flex flex-col gap-2">
+    <div>
+      <WalletRequestEventApprovalWallet/>
+    </div>
+
+    <div v-if="microblockToApprove">
+      <Card>
+        <template #content >
+          <div class="flex gap-4 flex-col">
+            <div>
+              <h1 class="text-xl font-bold">Microblock to approve</h1>
+              <p>Below is presented the microblock the application wants you to approve.</p>
+            </div>
+
+            <div>
+              <h2 class="text-lg font-bold">Microblock Header</h2>
+              <p>Hash: {{microblockToApprove.getHash().encode()}}</p>
+              <p>Height: {{microblockToApprove.getHeight()}}</p>
+            </div>
+
+            <div>
+              <h2 class="text-lg font-bold">Microblock Body</h2>
+              <div class="flex flex-col gap-2">
+                <div v-for="section of microblockToApprove.getAllSections()">
+                  <code class="block p-2 bg-gray-100 rounded-md overflow-hidden">
+                    {{JSON.stringify(section)}}
+                  </code>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </template>
+      </Card>
+
+    </div>
+
+    <div v-if="virtualBlockchainContainingMicroblock">
+      <Card>
+        <template #content>
+          <h1>Virtual blockchain</h1>
+          {{virtualBlockchainContainingMicroblock.getId()}}
+        </template>
+      </Card>
+    </div>
+  </div>
 </template>
